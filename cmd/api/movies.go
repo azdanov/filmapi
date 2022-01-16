a4939e175bfb0fdf7b3c00a44a2d3b0626268783
Anton Ždanov <azdanov@pm.me>
2022-01-16 22:28:49 +0200

    add json validation

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"time"
 
+	"filmapi.azdanov.dev/internal/validator"
+
 	"filmapi.azdanov.dev/internal/data"
 )
 
@@ -19,6 +21,20 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
