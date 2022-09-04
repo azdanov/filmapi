de5ae0cbca1d0d240626dcf33bf180de1ea57f78
Anton Ždanov <anton@azdanov.dev>
2022-09-04 20:43:12 +0300

    add cors middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -35,6 +36,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -59,6 +63,10 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "32cb8a7e024879", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "260798f911e5b7", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "FilmApi <no-reply@filmapi.io>", "SMTP sender")
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
